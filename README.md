5fa70104dccc4064bb8de2b16be1996eacef6ffd
karthikbokkesam <kbokkesam@gmail.com>
2024-04-14 14:40:07 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+<div align="center">
+    <a href="https://bkarthik.pro"><h1 align="center">bkarthik.pro</h1></a>
+
+My personal website, built with [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/), [Upstash](https://upstash.com), [Contentlayer](https://www.contentlayer.dev/) and deployed to [Vercel](https://vercel.com/).
+
+</div>
+
+<br/>
+
+## Running Locally
+
+
+```sh-session
+git clone https://github.com/karthikbokkesam/bkarthik.pro.git
+cd bkarthik.pro
+```
+
+
+Create a `.env` file similar to [`.env.example`](https://github.com/karthikbokkesam/bkarthik.pro/blob/main/.env.example).
+
+Then install dependencies and run the development server:
+```sh-session
+pnpm install
+pnpm dev
+```
+
+
+## Cloning / Forking
+
+Please remove all of my personal information (projects, images, etc.) before deploying your own version of this site.
---
+++ app/layout.tsx
@@ -0,0 +1,61 @@
+import "../global.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import type { Metadata } from "next";
+import { Analytics } from "./components/analytics";
+
+export const metadata: Metadata = {
+	title: {
+		default: "bkarthik.pro",
+		template: "%s | bkarthik.pro",
+	},
+	description: "Full-stack developer",
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+	twitter: {
+		title: "Karthik Bokkesam",
+		card: "summary_large_image",
+	},
+	icons: {
+		shortcut: "/favicon.png",
+	},
+};
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+	src: "../public/fonts/CalSans-SemiBold.ttf",
+	variable: "--font-calsans",
+});
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+			<head>
+				<Analytics />
+			</head>
+			<body
+				className={`bg-black ${
+					process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+				}`}
+			>
+				{children}
+			</body>
+		</html>
+	);
+}
